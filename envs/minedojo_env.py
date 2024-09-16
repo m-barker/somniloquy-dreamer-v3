d49b5eee31cdf941f56f877144b82d5d4a17321f
Matt <mattbarker322@gmail.com>
2024-09-16 08:46:56 +0100

    feat: nav reward wrapper for minedojo

--- envs/minedojo_env.py
+++ envs/minedojo_env.py
@@ -11,7 +11,11 @@ from typing import Literal
 
 class MineDojoEnv(gym.Env):
     def __init__(
-        self, task_id="harvest_1_dirt", image_size=(64, 64), world_seed: int = 128
+        self,
+        task_id="harvest_1_dirt",
+        image_size=(64, 64),
+        world_seed: int = 128,
+        log: bool = True,
     ):
         self.task_id = task_id
         self.image_size = image_size
@@ -19,6 +23,11 @@ class MineDojoEnv(gym.Env):
         self.env = minedojo.make(
             task_id=task_id, image_size=(160, 256), world_seed=world_seed
         )
+        self.log = log
+        if "milk" in task_id:
+            self.env = AnimalZooDenseRewardWrapper(
+                self.env, entity="cow", step_penalty=0, nav_reward_scale=0.1
+            )
         self.action_size = 89  # following frome MineCLIP implementation
         self.sticky_action_length = 30
         self._sticky_attack_counter = 0
@@ -142,7 +151,9 @@ class MineDojoEnv(gym.Env):
 
 
 class AnimalZooDenseRewardWrapper(Wrapper):
-    """From MineCLIP implementation"""
+    """Modified from MineCLIP implementation
+    https://github.com/MineDojo/MineCLIP/tree/main/mineclip/mineclip
+    """
 
     def __init__(
         self,
@@ -150,7 +161,6 @@ class AnimalZooDenseRewardWrapper(Wrapper):
         entity: Literal["cow", "sheep"],
         step_penalty: float | int,
         nav_reward_scale: float | int,
-        attack_reward: float | int,
     ):
         assert (
             "rays" in env.observation_space.keys()
@@ -161,55 +171,36 @@ class AnimalZooDenseRewardWrapper(Wrapper):
         assert step_penalty >= 0, f"penalty must be non-negative"
         self._step_penalty = step_penalty
         self._nav_reward_scale = nav_reward_scale
-        self._attack_reward = attack_reward
 
-        self._weapon_durability_deque = deque(maxlen=2)  # type: ignore
         self._consecutive_distances = deque(maxlen=2)  # type: ignore
-        self._distance_min = np.inf
 
     def reset(self, **kwargs):
-        self._weapon_durability_deque.clear()
         self._consecutive_distances.clear()
-        self._distance_min = np.inf
 
         obs = super().reset(**kwargs)
 
         entity_in_sight, distance = self._find_distance_to_entity_if_in_sight(obs)
         if entity_in_sight:
-            distance = self._distance_min = min(distance, self._distance_min)
             self._consecutive_distances.append(distance)
         else:
             self._consecutive_distances.append(0)
-        self._weapon_durability_deque.append(obs["inventory"]["cur_durability"][0])
 
         return obs
 
     def step(self, action):
         obs, _reward, done, info = super().step(action)
 
-        self._weapon_durability_deque.append(obs["inventory"]["cur_durability"][0])
-        valid_attack = (
-            self._weapon_durability_deque[0] - self._weapon_durability_deque[1]
-        )
-        # when dying, the weapon is gone and durability changes to 0
-        valid_attack = 1.0 if valid_attack == 1.0 else 0.0
-
-        # attack reward
-        attack_reward = valid_attack * self._attack_reward
         # nav reward
         entity_in_sight, distance = self._find_distance_to_entity_if_in_sight(obs)
         nav_reward = 0
         if entity_in_sight:
-            distance = self._distance_min = min(distance, self._distance_min)
             self._consecutive_distances.append(distance)
             nav_reward = self._consecutive_distances[0] - self._consecutive_distances[1]
         nav_reward = max(0, nav_reward)
         nav_reward *= self._nav_reward_scale
-        # reset distance min if attacking the entity because entity will run away
-        if valid_attack > 0:
-            self._distance_min = np.inf
-        # total reward
-        reward = attack_reward + nav_reward - self._step_penalty + _reward
+        if self.log:
+            print(f"nav_reward: {nav_reward}")
+        reward = nav_reward - self._step_penalty + _reward
         return obs, reward, done, info
 
     def _find_distance_to_entity_if_in_sight(self, obs):
