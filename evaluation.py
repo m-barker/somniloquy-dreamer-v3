fix: crafter reconstructio normalisation

evaluation.py
@@ -1230,7 +1230,7 @@ def crafter_narration_using_obs_reconstruction(
                     .cpu()
                     .numpy()
                     .flatten()
-                    * 500
+                    * 50
                 ).astype(np.uint8)
                 for state in imagined_states
             ]
@@ -1241,7 +1241,7 @@ def crafter_narration_using_obs_reconstruction(
                     .cpu()
                     .numpy()
                     .flatten()
-                    * 500
+                    * 50
                 ).astype(np.uint8)
                 for state in imagined_states
             ]
@@ -1264,7 +1264,7 @@ def crafter_narration_using_obs_reconstruction(
                     .cpu()
                     .numpy()
                     .flatten()
-                    * 500
+                    * 50
                 ).astype(np.uint8)
                 for state in posterior_states
             ]
@@ -1275,7 +1275,7 @@ def crafter_narration_using_obs_reconstruction(
                     .cpu()
                     .numpy()
                     .flatten()
-                    * 500
+                    * 50
                 ).astype(np.uint8)
                 for state in posterior_states
             ]