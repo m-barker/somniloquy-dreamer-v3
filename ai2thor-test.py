746e240e90d5c1bb098652e07c8c77e861d41561
Matt <mattbarker322@gmail.com>
2025-04-17 19:00:09 +0100

    test: ai2thor headless clound render test script

---
+++ ai2thor-test.py
@@ -0,0 +1,13 @@
+# From https://github.com/allenai/ai2thor-docker/blob/main/example_agent.py
+# Tests that cloud rendering works
+import ai2thor.controller
+import ai2thor.platform
+from pprint import pprint
+
+
+if __name__ == "__main__":
+    controller = ai2thor.controller.Controller(
+        platform=ai2thor.platform.CloudRendering, scene="FloorPlan28"
+    )
+    event = controller.step(action="RotateRight")
+    pprint(event.metadata["agent"])
