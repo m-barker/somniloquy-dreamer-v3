2bc822964fbc0b5d237291f367dee14f979499bd
Matt <mattbarker322@gmail.com>
2025-04-21 12:19:17 +0100

    fix: better ai2thor performance tracking

--- ai2thor-test.py
+++ ai2thor-test.py
@@ -2,16 +2,21 @@
 # Tests that cloud rendering works
 import ai2thor.controller
 import ai2thor.platform
+import time
 from pprint import pprint
 
 
 if __name__ == "__main__":
     controller = ai2thor.controller.Controller(
-        platform=ai2thor.platform.CloudRendering, scene="FloorPlan28"
+        platform=ai2thor.platform.CloudRendering,
+        scene="FloorPlan28",
     )
     step_count = 0
+    start = time.time()
     while True:
         event = controller.step(action="RotateRight")
         step_count += 1
-        print(f"Step: {step_count}")
+        if step_count % 100 == 0:
+            print(f"Time for 100 steps: {time.time() - start}")
+            start = time.time()
     pprint(event.metadata["agent"])
