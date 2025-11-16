f66da27e02d063c3ed1df326ba892056d9181300
Matt Barker <mattbarker322@gmail.com>
2025-11-16 11:31:46 +0000

    fix: handle custom eval metrics in printing eval string

--- evaluation.py
+++ evaluation.py
@@ -1259,6 +1259,7 @@ def get_eval_summary_string(
     imagined_metrics: Dict[str, float],
     reconstructed_metrics: Dict[str, float],
     training_step: int,
+    metrics: List[str] = "bleu",
 ) -> str:
     """Generates a string summarising this epoch's evaluation
     results.
@@ -1281,52 +1282,72 @@ def get_eval_summary_string(
 
     summary_string += "Imagined Plan Metrics with Stopwords:\n"
     summary_string += "-" * 50 + "\n"
-    summary_string += (
-        f"\t  Mean Imagined BLEU: {imagined_metrics['mean_imagined_bleu_score']:.2f}\n"
-    )
-    summary_string += (
-        f"\t  Mean Imagined METEOR: {imagined_metrics['mean_imagined_meteor']:.2f}\n"
-    )
-    summary_string += f"\t  Mean Imagined ROUGE 1 F: {imagined_metrics['mean_imagined_rouge1_fmeasure']:.2f}\n"
-    summary_string += (
-        f"\t  Mean Imagined WER: {imagined_metrics['mean_imagined_wer']:.2f}\n"
-    )
-    summary_string += (
-        f"\t  Mean Imagined TER: {imagined_metrics['mean_imagined_ter']:.2f}\n"
-    )
-    summary_string += (
-        f"\t  Mean Imagined chrF++: {imagined_metrics['mean_imagined_chrf']:.2f}\n"
-    )
+    if "bleu" in metrics:
+        summary_string += f"\t  Mean Imagined BLEU: {imagined_metrics['mean_imagined_bleu_score']:.2f}\n"
+    if "meteor" in metrics:
+        summary_string += f"\t  Mean Imagined METEOR: {imagined_metrics['mean_imagined_meteor']:.2f}\n"
+    if "rogue" in metrics:
+        summary_string += f"\t  Mean Imagined ROUGE 1 F: {imagined_metrics['mean_imagined_rouge1_fmeasure']:.2f}\n"
+    if "wer" in metrics:
+        summary_string += (
+            f"\t  Mean Imagined WER: {imagined_metrics['mean_imagined_wer']:.2f}\n"
+        )
+    if "ter" in metrics:
+        summary_string += (
+            f"\t  Mean Imagined TER: {imagined_metrics['mean_imagined_ter']:.2f}\n"
+        )
+    if "chrf" in metrics:
+        summary_string += (
+            f"\t  Mean Imagined chrF++: {imagined_metrics['mean_imagined_chrf']:.2f}\n"
+        )
     summary_string += "-" * 50 + "\n"
 
     summary_string += "Reconstructed Plan Metrics with Stopwords:\n"
     summary_string += "-" * 50 + "\n"
-    summary_string += f"\t  Mean Reconstructed BLEU: {reconstructed_metrics['mean_reconstructed_bleu_score']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed METEOR: {reconstructed_metrics['mean_reconstructed_meteor']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed ROUGE 1 F: {reconstructed_metrics['mean_reconstructed_rouge1_fmeasure']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed WER: {reconstructed_metrics['mean_reconstructed_wer']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed TER: {reconstructed_metrics['mean_reconstructed_ter']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed chrF++: {reconstructed_metrics['mean_reconstructed_chrf']:.2f}\n"
+    if "bleu" in metrics:
+        summary_string += f"\t  Mean Reconstructed BLEU: {reconstructed_metrics['mean_reconstructed_bleu_score']:.2f}\n"
+    if "meteor" in metrics:
+        summary_string += f"\t  Mean Reconstructed METEOR: {reconstructed_metrics['mean_reconstructed_meteor']:.2f}\n"
+    if "rouge" in metrics:
+        summary_string += f"\t  Mean Reconstructed ROUGE 1 F: {reconstructed_metrics['mean_reconstructed_rouge1_fmeasure']:.2f}\n"
+    if "wer" in metrics:
+        summary_string += f"\t  Mean Reconstructed WER: {reconstructed_metrics['mean_reconstructed_wer']:.2f}\n"
+    if "ter" in metrics:
+        summary_string += f"\t  Mean Reconstructed TER: {reconstructed_metrics['mean_reconstructed_ter']:.2f}\n"
+    if "chrf" in metrics:
+        summary_string += f"\t  Mean Reconstructed chrF++: {reconstructed_metrics['mean_reconstructed_chrf']:.2f}\n"
     summary_string += "-" * 50 + "\n"
 
     summary_string += "Imagined Plan Metrics without Stopwords:\n"
     summary_string += "-" * 50 + "\n"
-    summary_string += f"\t  Mean Imagined BLEU: {imagined_metrics['mean_imagined_bleu_score_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Imagined METEOR: {imagined_metrics['mean_imagined_meteor_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Imagined ROUGE 1 F: {imagined_metrics['mean_imagined_rouge1_fmeasure_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Imagined WER: {imagined_metrics['mean_imagined_wer_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Imagined TER: {imagined_metrics['mean_imagined_ter_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Imagined chrF++: {imagined_metrics['mean_imagined_chrf_no_stopwords']:.2f}\n"
+    if "bleu" in metrics:
+        summary_string += f"\t  Mean Imagined BLEU: {imagined_metrics['mean_imagined_bleu_score_no_stopwords']:.2f}\n"
+    if "meteor" in metrics:
+        summary_string += f"\t  Mean Imagined METEOR: {imagined_metrics['mean_imagined_meteor_no_stopwords']:.2f}\n"
+    if "rougue" in metrics:
+        summary_string += f"\t  Mean Imagined ROUGE 1 F: {imagined_metrics['mean_imagined_rouge1_fmeasure_no_stopwords']:.2f}\n"
+    if "wer" in metrics:
+        summary_string += f"\t  Mean Imagined WER: {imagined_metrics['mean_imagined_wer_no_stopwords']:.2f}\n"
+    if "ter" in metrics:
+        summary_string += f"\t  Mean Imagined TER: {imagined_metrics['mean_imagined_ter_no_stopwords']:.2f}\n"
+    if "chrf" in metrics:
+        summary_string += f"\t  Mean Imagined chrF++: {imagined_metrics['mean_imagined_chrf_no_stopwords']:.2f}\n"
     summary_string += "-" * 50 + "\n"
 
     summary_string += "Reconstructed Plan Metrics without Stopwords:\n"
     summary_string += "-" * 50 + "\n"
-    summary_string += f"\t  Mean Reconstructed BLEU: {reconstructed_metrics['mean_reconstructed_bleu_score_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed METEOR: {reconstructed_metrics['mean_reconstructed_meteor_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed ROUGE 1 F: {reconstructed_metrics['mean_reconstructed_rouge1_fmeasure_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed WER: {reconstructed_metrics['mean_reconstructed_wer_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed TER: {reconstructed_metrics['mean_reconstructed_ter_no_stopwords']:.2f}\n"
-    summary_string += f"\t  Mean Reconstructed chrF++: {reconstructed_metrics['mean_reconstructed_chrf_no_stopwords']:.2f}\n"
+    if "bleu" in metrics:
+        summary_string += f"\t  Mean Reconstructed BLEU: {reconstructed_metrics['mean_reconstructed_bleu_score_no_stopwords']:.2f}\n"
+    if "meteor" in metrics:
+        summary_string += f"\t  Mean Reconstructed METEOR: {reconstructed_metrics['mean_reconstructed_meteor_no_stopwords']:.2f}\n"
+    if "rouge" in metrics:
+        summary_string += f"\t  Mean Reconstructed ROUGE 1 F: {reconstructed_metrics['mean_reconstructed_rouge1_fmeasure_no_stopwords']:.2f}\n"
+    if "wer" in metrics:
+        summary_string += f"\t  Mean Reconstructed WER: {reconstructed_metrics['mean_reconstructed_wer_no_stopwords']:.2f}\n"
+    if "ter" in metrics:
+        summary_string += f"\t  Mean Reconstructed TER: {reconstructed_metrics['mean_reconstructed_ter_no_stopwords']:.2f}\n"
+    if "chrf" in metrics:
+        summary_string += f"\t  Mean Reconstructed chrF++: {reconstructed_metrics['mean_reconstructed_chrf_no_stopwords']:.2f}\n"
     summary_string += "-" * 50 + "\n"
 
     return summary_string
@@ -1517,7 +1538,10 @@ def evaluate_rollouts(
 
         if config.log_translation_eval:
             summary_string = get_eval_summary_string(
-                imagined_metrics, reconstructed_metrics, logger.step
+                imagined_metrics,
+                reconstructed_metrics,
+                logger.step,
+                config.translation_metrics,
             )
             summary_string += f"Mean eval episode return: {np.mean(episode_rewards)}"
             print(summary_string)
